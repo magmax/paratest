a48f06e2c1765f0109197bb7564823eb924db06c
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-02-23 08:20:06 +0100

    Adding setup/teardown scripts

--- paratest.py
+++ paratest.py
@@ -1,11 +1,17 @@
+import sys
 import argparse
 import queue
 import threading
+import subprocess
 import logging
 from yapsy.PluginManager import PluginManager
 
 logger = logging.getLogger(__name__)
 shared_queue = queue.Queue()
+
+
+class Abort(Exception):
+    pass
 
 
 def configure_logging(verbosity):
@@ -16,6 +22,7 @@ def configure_logging(verbosity):
     handler.setFormatter(formatter)
     logger.addHandler(handler)
     logger.setLevel(level)
+
 
 def main():
     parser = argparse.ArgumentParser(description='Run tests in parallel')
@@ -39,6 +46,7 @@ def main():
     )
     parser.add_argument(
         '-n', '--environments',
+        type=int,
         default=5,
         help="Number of environments to be created",
     )
@@ -48,10 +56,50 @@ def main():
         default=0,
         help='Increase the verbosity level'
     )
+    parser.add_argument(
+        '--setup',
+        help='Script to prepare everything; it will be run once at the beginning'
+    )
+    parser.add_argument(
+        '--setup-workspace',
+        dest='setup_workspace',
+        help='Script to prepare the workspace; it will be run once by worker',
+    )
+    parser.add_argument(
+        '--setup-test',
+        dest='setup_test',
+        help='Script to prepare a test; it will be run before each test'
+    )
+    parser.add_argument(
+        '--teardown-test',
+        dest='teardown_test',
+        help='Script to finalize a test; it will be run after each test'
+    )
+    parser.add_argument(
+        '--teardown-workspace',
+        dest='teardown_workspace',
+        help='Script to finalize a workspace; it will be run once by worker when no more tests are available'
+    )
+    parser.add_argument(
+        '--teardown',
+        help='Script to finalize; it will be run once at the end'
+    )
 
     args = parser.parse_args()
     configure_logging(args.verbosity)
-    process(args)
+    return process(args)
+
+
+def run_script(script):
+    if not script:
+        return
+    logger.debug("About to run script %s", script)
+    result = subprocess.run(script, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if result.stdout:
+        logger.debug(result.stdout)
+    if result.stderr:
+        logger.warning(result.stderr)
+    return result.returncode
 
 
 def process(args):
@@ -71,6 +119,8 @@ def process(args):
         plugin = simplePluginManager.getPluginByName(args.plugin)
         po = plugin.plugin_object
 
+        if run_script(args.setup):
+            raise Abort('The setup script failed. aborting.')
         workers = []
         tids = 0
         for tid in po.find(args.path):
@@ -78,33 +128,55 @@ def process(args):
             tids += 1
 
         for i in range(min(args.environments, tids)):
-            t = Worker(po, name=str(i))
+            t = Worker(
+                po,
+                setup=args.setup_workspace,
+                setup_test=args.setup_test,
+                teardown_test=args.teardown_test,
+                teardown=args.teardown_workspace,
+                name=str(i),
+            )
             workers.append(t)
 
         logger.debug("start workers")
         for t in workers:
             t.start()
             shared_queue.put(None)
-        logger.debug("wait for all events to be processed")
-        shared_queue.join()
-        logger.debug("wait for all workers")
+        logger.debug("wait for all workers to finish")
         for t in workers:
             t.join()
+        if run_script(args.teardown):
+            raise Abort('The teardown script failed, but nothing can be done.')
+
+        if not shared_queue.empty():
+            raise Abort('There were unprocessed tests, but all workers are dead. Aborting.')
 
 
 class Worker(threading.Thread):
-    def __init__(self, plugin, *args, **kwargs):
+    def __init__(self, plugin, setup, setup_test, teardown, teardown_test, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.plugin = plugin
+        self.setup = setup
+        self.setup_test = setup_test
+        self.teardown = teardown
+        self.teardown_test = teardown_test
 
     def run(self):
         logger.debug("%s START" % self.name)
         self.plugin.init_environment(1)
         item = object()
+        if run_script(self.setup):
+            raise Abort('Setup workspace failed on worker %s and could not initialize the environment. Worker is dead')
         while item:
+            if run_script(self.setup_test):
+                raise Abort("setup_test failed on worker %s. Worker is dead", self.name)
             item = shared_queue.get()
             self.process(item)
             shared_queue.task_done()
+            if run_script(self.teardown_test):
+                raise Abort("teardown_test failed on worker %s. Worker is dead", self.name)
+        if run_script(self.teardown):
+            raise Abort('Teardown workspace failed on worker %s. Worker is dead')
 
     def process(self, tid):
         if tid is None:
@@ -116,4 +188,8 @@ class Worker(threading.Thread):
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        main()
+    except Abort as e:
+        logger.critical(e)
+        sys.exit(2)
