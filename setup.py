faf8137e584497df96a06f0b4a51cf30473afedd
Miguel Angel Garcia <miguelangel.garcia@gmail.com>
2016-04-07 07:00:14 +0200

    Fixing flakes

--- setup.py
+++ setup.py
@@ -32,39 +32,40 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
-setup(name='paratest',
-      version=__version__,
-      description=(
-          "Test paralelizer"
-      ),
-      long_description=read_description(),
-      cmdclass={'test': PyTest},
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Topic :: Software Development :: User Interfaces',
-          'Topic :: Software Development :: '
-          'Libraries :: Application Frameworks',
-      ],
-      keywords='parallel test',
-      author='Miguel Ángel García',
-      author_email='miguelangel.garcia@gmail.com',
-      url='https://github.com/magmax/paratest',
-      license='MIT',
-      packages=find_packages(exclude=['tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'yapsy == 1.11.223',
-      ],
-      tests_require=[
-          'pytest',
-          'pytest-cov',
-      ],
+setup(
+    name='paratest',
+    version=__version__,
+    description=(
+        "Test paralelizer"
+    ),
+    long_description=read_description(),
+    cmdclass={'test': PyTest},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Software Development :: '
+        'Libraries :: Application Frameworks',
+    ],
+    keywords='parallel test',
+    author='Miguel Ángel García',
+    author_email='miguelangel.garcia@gmail.com',
+    url='https://github.com/magmax/paratest',
+    license='MIT',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'yapsy == 1.11.223',
+    ],
+    tests_require=[
+        'pytest',
+        'pytest-cov',
+    ],
 )
